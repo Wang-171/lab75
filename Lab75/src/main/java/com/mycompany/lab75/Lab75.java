42739952d573b116183c84e77a3c2198955c0191
nguyendangQ <quangviplang@gmail.com>
2025-03-12 16:41:00 +0700

    Create

---
+++ Lab75/src/main/java/com/mycompany/lab75/Lab75.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab75;
+
+/**
+ *
+ * @author quang
+ */
+public class Lab75 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
